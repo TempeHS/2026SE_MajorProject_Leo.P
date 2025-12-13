6268ed16ecb335604462895bd54bb5383b19d644
Leo P <leo.projceski@education.nsw.gov.au>
2025-12-13 07:27:08 +0000

    wip: working on registration system

--- userManagement.py
+++ userManagement.py
@@ -6,7 +6,16 @@ import bcrypt
 def getUsers():
     con = sql.connect("databaseFiles/database.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM id7-tusers")
+    cur.execute("SELECT * FROM user_info  ")
     con.close()
     return cur
 
+
+def insertSignup(email, password):
+    con = sql.connect("databaseFiles/database.db")
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO user_info (email, password) VALUES (?,?)", (email, password)
+    )
+    con.commit()
+    con.close()
