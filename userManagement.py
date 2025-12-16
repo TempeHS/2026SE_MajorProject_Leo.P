608af95a0123f868d5e9b3edbbd4d4aa17767e83
Leo P <leo.projceski@education.nsw.gov.au>
2025-12-16 09:54:17 +0000

    feature: password hashing (salt bcrypt)

--- userManagement.py
+++ userManagement.py
@@ -12,16 +12,27 @@ def getUsers(email, password):
     con.close()
     if result is None:
         return False
-    else:
-        return True
+    stored_password = result[0]
+    password_bytes = password.encode("utf-8")
+    if isinstance(stored_password, str):
+        stored_password = stored_password.encode("utf-8")
+
+    return bcrypt.checkpw(password_bytes, stored_password)
 
 
 def insertSignup(email, password):
     con = sql.connect("databaseFiles/database.db")
     cur = con.cursor()
+
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    hashed_password_str = hashed_password.decode("utf-8")
+
     try:
         cur.execute(
-            "INSERT INTO user_info (email, password) VALUES (?,?)", (email, password)
+            "INSERT INTO user_info (email, password) VALUES (?,?)",
+            (email, hashed_password_str),
         )
         con.commit()
         con.close()
